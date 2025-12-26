825e1b9c233226120b04ef06e8e27faf8f440de3
Jeyaraj Rahul <your@email.com>
2025-12-26 15:19:01 +0530

    made a change

--- voicetxt.py
+++ voicetxt.py
@@ -50,6 +50,9 @@ def index():
 
     return render_template("index.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
